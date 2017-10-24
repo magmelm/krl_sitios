8545544ec5a84dc8c25852cb190e7133e997b0f0
mag <mag@mag-VirtualBox>
2017-10-24 10:57:59 -0500

    Se agrega link a GitHub

--- sitios/default.php
+++ sitios/default.php
@@ -152,6 +152,7 @@ img:hover{
 	
 	<a href="http://gnula.nu"><img src="images/gnula.png" alt="Gnula"></a>
 	<a href="http://web.whatsapp.com"><img src="images/whatsapp.jpg" alt="WhatsApp"></a>
+	<a href="https://github.com/login"><img src="images/github.png" alt="GitHub"></a>
 	
 	<a href="http://www.krl.mx/"><img src="images/krlmx.jpg" alt="krl.mx"></a>
 	
