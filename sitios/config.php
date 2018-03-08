ee5f8ca0f3f8da4a7429a614617309b45f679ecd
magmelm <magmelm@gmail.com>
2018-03-07 20:14:25 -0500

    Se agrega archivo de configuración

---
+++ sitios/config.php
@@ -0,0 +1,12 @@
+
+
+<?php
+    return [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'db_name' => 'test',
+    	'table_name' => 'test.sitios'
+    ];
+?>
+
